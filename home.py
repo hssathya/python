55b838fbb5bdafb31499eb7a8532cd0aeab34fd9
Sathya <hssathya@gmail.com>
2021-05-03 16:18:19 +0530

    initial version - shashank

---
+++ home.py
@@ -0,0 +1,15 @@
+#squre
+import turtle
+
+t = turtle.Pen()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(45)
+t.forward(73)
+t.right(90)
+t.forward(73)
